474bac6668f230d34930bb466f51985836c00ef9
PopCristianGabriel <popcristian9898@yahoo.com>
2020-08-15 21:42:43 +0300

    Iteration1

---
+++ testRobot2.py
@@ -0,0 +1,12 @@
+import asyncio
+import websockets
+
+async def hello(websocket,path):
+    name = await websocket.recv()
+    print(name)
+
+start_server = websockets.serve(hello, "localhost",8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+
+asyncio.get_event_loop().run_forever()
