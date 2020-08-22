ae53568e8793eafe27aeb03ea1b8b545c32e75f3
Alex Dragus <alexdragus14@alex.dragus>
2020-08-22 18:22:37 +0300

    Prima intalnire

--- testRobot2.py
+++ testRobot2.py
@@ -5,7 +5,7 @@ async def hello(websocket,path):
     name = await websocket.recv()
     print(name)
 
-start_server = websockets.serve(hello, "localhost",8765)
+start_server = websockets.serve(hello, "192.168.43.51", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 
